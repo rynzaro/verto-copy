fdd3887c18a7502327d65268f874101f4113c060
rynzaro <rynzaro44@gmail.com>
2024-06-24 09:14:56 +0200

    add token icons2

--- lib/availableTokens.ts
+++ lib/availableTokens.ts
@@ -1,7 +1,12 @@
+'use client'
+
+import { useNearWallet } from "@/providers/wallet";
+import { useEffect } from 'react';
+const { viewMethod } = useNearWallet();
+
 export const availableTokens = [
     { name: "near", symbol: "NEAR", contractId: "near", icon: ''},
     { name: "usdc", symbol: "USDC", contractId: "usdc.fakes.testnet", icon: ''},
     { name: "aurora", symbol: "AURORA", contractId: "aurora.fakes.testnet", icon: ''},
     { name: "eth", symbol: "ETH", contractId: "eth.fakes.testnet", icon: ''}
 ]
-
