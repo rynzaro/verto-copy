95c5d79684598b7e840146382366a54eb43babde
rynzaro <rynzaro44@gmail.com>
2024-07-05 12:57:08 +0200

    finalize toggle

--- components/forms/FilterForm.tsx
+++ components/forms/FilterForm.tsx
@@ -104,24 +104,26 @@ export default function FilterForm({
                     Filter
                 </span>
             </button>
-            {showCompletedToggle ?  
-            <Field className="flex items-center relative text-white mx-4">
-                <Switch
-                    checked={values.showCompleted}
-                    onChange={toggleShowCompleted}
-                    className="group relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 data-[checked]:bg-indigo-600"
-                >
-                    <span
-                        aria-hidden="true"
-                        className="pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out group-data-[checked]:translate-x-5"
-                    />
-                </Switch>
-                <Label as="span" className="ml-3 text-sm ">
-                    <span className="font-medium text-white">Show Completed</span>{' '}
-                </Label>
-            </Field>
-            :
-            <></>
+            {showCompletedToggle ?
+                <Field className="flex items-center relative text-white mx-4">
+                    <Switch
+                        checked={values.showCompleted}
+                        onChange={toggleShowCompleted}
+                        className="group relative inline-flex items-center h-8 w-16 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-600 transition-colors duration-200 ease-in-out  data-[checked]:bg-lime-300"
+                    >
+                        <span
+                            aria-hidden="true"
+                            className={`pointer-events-none inline-block h-5 w-5 transform rounded-full bg-gray-900 shadow ring-0 transition duration-200  ease-in-out ${values.showCompleted ? 'translate-x-9' : 'translate-x-1'}`} // Adjusted translate values
+                        />
+                    </Switch>
+                    <Label as="span" className="ml-3 text-sm ">
+                        <span className="font-medium text-white">Show Completed Orders</span>{' '}
+                    </Label>
+                </Field>
+
+
+                :
+                <></>
             }
         </div>
     )
